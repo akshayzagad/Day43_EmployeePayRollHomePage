13c028e56087f3fd3086f9049616596eddcd7299
akshayzagad <akshayzagade62@gmail.com.com>
2023-01-20 13:48:36 +0530

    Solve the error in Date

--- utility.js
+++ utility.js
@@ -1,6 +1,7 @@
 const stringifyDate = (date) => {
     const options = {day:'numeric',month:'short',year:'numeric'};
+    console.log(date+ " ok ")
     const newDate = !date ? "undefined" :
-                    new Date(Date.parse(date)).toLocaleDateString('en-GB',options);
+                    new Date(date).toLocaleDateString('en-GB',options);
     return newDate; 
 }
